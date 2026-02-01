22f005bcf85a0bc48a4a7caab3ee56f0ed8ba4df
divyanshu patel <divyanshupatel15243@gmail.com>
2026-02-02 00:45:03 +0530

    first commit

---
+++ dist/@/assets/LabelArea-CgR2bQcr.js
@@ -0,0 +1,1 @@
+import{j as t,h as a}from"./index-CpI_fUYm.js";const o=({label:s})=>t.jsx(a.Label,{className:"col-span-4 sm:col-span-2 font-medium text-sm",children:s});export{o as L};
---
+++ dist/@/assets/useDisableForDemo-BPiYJQrx.js
@@ -0,0 +1,1 @@
+import"./index-CpI_fUYm.js";import"./toast-CnFrhWPE.js";const s=()=>({handleDisableForDemo:()=>!1});export{s as u};
